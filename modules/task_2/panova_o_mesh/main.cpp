89515f5fdfd606000023533ddb9d6ef580b36e96
OlgaMakesThings <56171499+OlgaMakesThings@users.noreply.github.com>
2020-12-08 19:15:27 +0300

    after revert

--- modules/task_2/panova_o_mesh/main.cpp
+++ modules/task_2/panova_o_mesh/main.cpp
@@ -4,6 +4,18 @@
 #include <mpi.h>
 #include <iostream>
 #include "../../../modules/task_2/panova_o_mesh/mesh.h"
+TEST(Creature, TheMeshWasCreatedCorrectly) {
+    int rank, size;
+    int dims[2], coords[2], periods[2];
+    MPI_Comm test_comm = CreateMesh(2);
+    MPI_Comm_rank(test_comm, &rank);
+    MPI_Comm_size(test_comm, &size);
+    int ndims;
+    MPI_Cartdim_get(test_comm, &ndims);
+    MPI_Cart_get(test_comm, ndims, dims, periods, coords);
+    EXPECT_TRUE(periods[0] & periods[1]);
+    ASSERT_EQ(ndims, 2);
+}
 TEST(Transmission, SendDataInt_From1ToLast_Dims2) {
     int s_buf = 12345;
     int* f_buf = new int[100];
@@ -42,27 +54,6 @@ TEST(Transmission, SendDataInt_FromAToB_Dims2) {
         SendRecvIntData(s_buf, ndims, A, B, f_buf);
         if (rank == B) {
             ASSERT_EQ(s_buf, f_buf[0]);
-        }
-    }
-}
-TEST(Transmission, SendDataPacked_FromLastTo1_Dims2) {
-    int ndims = 2;
-    int int_data = 12345;
-    double doub_data = 12.00045389;
-    double doub_data1 = 1.4512654687;
-    MPI_Comm test_comm = CreateMesh(ndims);
-    int rank, size;
-    MPI_Comm_rank(test_comm, &rank);
-    MPI_Comm_size(test_comm, &size);
-    if (size == 1) {
-        ASSERT_EQ(1, 1);
-    } else {
-        bool was = true;
-        int f_rank = 0;
-        int s_rank = size - 1;
-        SendRecvPackedData(int_data, doub_data, doub_data1, ndims, s_rank, f_rank, was);
-        if (rank == f_rank) {
-            ASSERT_EQ(was, true);
         }
     }
 }
--- modules/task_2/panova_o_mesh/mesh.cpp
+++ modules/task_2/panova_o_mesh/mesh.cpp
@@ -69,33 +69,3 @@ void SendRecvDoubleData(double s_buf, int ndims, int s_rank, int f_rank, double*
     else if (rank == f_rank)
         RecvData(&f_buf[0], 1, MPI_DOUBLE, s_coords, test_comm, &status);
 }
-void SendRecvPackedData(int int_data, double doub_data, double doub_data1,
-    int ndims, int s_rank, int f_rank, bool was) {
-    int rank = 0, size = 0;
-    MPI_Comm test_comm = CreateMesh(ndims);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    int bufpos = 0;
-    void* buf[1000];
-    MPI_Status status;
-    int* s_coords = new int[ndims];
-    MPI_Cart_coords(test_comm, s_rank, ndims, s_coords);
-    int* f_coords = new int[ndims];
-    MPI_Cart_coords(test_comm, f_rank, ndims, f_coords);
-    if (rank == s_rank) {
-        MPI_Pack(&int_data, 1, MPI_INT, buf, 1000, &bufpos, test_comm);
-        MPI_Pack(&doub_data, 1, MPI_DOUBLE, buf, 1000, &bufpos, test_comm);
-        MPI_Pack(&doub_data1, 1, MPI_DOUBLE, buf, 1000, &bufpos, test_comm);
-        SendData(&buf, bufpos, MPI_PACKED, f_coords, test_comm);
-    } else if (rank == f_rank) {
-        RecvData(&buf, 20, MPI_PACKED, s_coords, test_comm, &status);
-        bufpos = 0;
-        int f_int_data = 0;
-        double f_doub_data = 0;
-        double f_doub_data1 = 0;
-        MPI_Unpack(buf, 1000, &bufpos, &f_int_data, 1, MPI_INT, test_comm);
-        MPI_Unpack(buf, 1000, &bufpos, &f_doub_data, 1, MPI_DOUBLE, test_comm);
-        MPI_Unpack(buf, 1000, &bufpos, &f_doub_data1, 1, MPI_DOUBLE, test_comm);
-        if (f_int_data == 0 || f_doub_data == 0 || f_doub_data1 == 0) was = false;
-    }
-}
